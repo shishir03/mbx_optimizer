84f1ddfdda4b2fa1c37dadda723bbc49ae92dd8a
Shishir Iyer <shiyer@ucsd.edu>
2025-01-31 18:24:47 -0800

    made the design less stupid

--- optimize_2b_file.py
+++ optimize_2b_file.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 from computations import reassign_shared_computations
 from convert_to_arr import convert_to_arr
@@ -10,74 +11,77 @@ from remove_newlines import remove_newlines
 from remove_unused import remove_unused_vars
 from howBIGisit import how_big_is_it
 
+MBX_DIR = f"{os.getenv("HOME")}/MBX/src/potential"
+files = []
+
 print("Removing newlines...")
 t1 = time.time()
-file1 = "src/potential/2b/poly-2b-v6x.cpp"
-file2 = "nobreaks.cpp"
-remove_newlines(file1, "new_file.txt")
-remove_newlines("new_file.txt", file2, "const double t[0-9]+ =[^;]*;")
+files.append(f"{MBX_DIR}/2b/poly-2b-v6x.cpp")
+files.append("out_files/nobreaks.cpp")
+remove_newlines(files[0], "out_files/new_file.txt")
+remove_newlines("out_files/new_file.txt", files[-1], "const double t[0-9]+ =[^;]*;")
 t2 = time.time()
-print(f"Removed newlines from {file1} in {t2 - t1} seconds")
+print(f"Removed newlines from {files[-2]} in {t2 - t1} seconds")
 
 print("Converting to t[] array...")
 t1 = time.time()
-file2_1 = "noconst.cpp"
-convert_to_arr(file2, file2_1)
+files.append("out_files/noconst.cpp")
+convert_to_arr(files[-2], files[-1])
 t2 = time.time()
-print(f"Converted {file2} to use t[] in {t2 - t1} seconds")
+print(f"Converted {files[-2]} to use t[] in {t2 - t1} seconds")
 
 print("Removing df[] array...")
 t1 = time.time()
-file2_2 = "nodf.cpp"
-remove_df(file2_1, file2_2)
+files.append("out_files/nodf.cpp")
+remove_df(files[-2], files[-1])
 t2 = time.time()
-print(f"Removed df[] from {file2_1} in {t2 - t1} seconds")
+print(f"Removed df[] from {files[-2]} in {t2 - t1} seconds")
 
 print("Removing a assigments...")
 t1 = time.time()
-file3 = "noaassignments.cpp"
-remove_assignments(file2_2, file3, "a")
+files.append("out_files/noaassignments.cpp")
+remove_assignments(files[-2], files[-1], "a")
 t2 = time.time()
-print(f"Removed a assignments from {file2} in {t2 - t1} seconds")
+print(f"Removed a assignments from {files[-2]} in {t2 - t1} seconds")
 
 print("Removing unused indices...")
 t1 = time.time()
-file4 = "noskips.cpp"
-remove_unused_vars(file3, file4)
+files.append("out_files/noskips.cpp")
+remove_unused_vars(files[-2], files[-1])
 t2 = time.time()
-print(f"Removed unused indices from {file3} in {t2 - t1} seconds")
+print(f"Removed unused indices from {files[-2]} in {t2 - t1} seconds")
 
 print("Reassigning shared computations (pass 1)...")
 t1 = time.time()
-file5 = "sharedcomps.cpp"
-reassign_shared_computations(file4, file5)
+files.append("out_files/sharedcomps.cpp")
+reassign_shared_computations(files[-2], files[-1])
 print("Reassigning shared computations (pass 2)...")
-file6 = "sharedcomps2.cpp"
-reassign_shared_computations(file5, file6)
+files.append("out_files/sharedcomps2.cpp")
+reassign_shared_computations(files[-2], files[-1])
 t2 = time.time()
-print(f"Reassigned shared computations in {file4} in {t2 - t1} seconds")
+print(f"Reassigned shared computations in {files[-2]} in {t2 - t1} seconds")
 
 print("Reassigning variables...")
 t1 = time.time()
-file7 = "newvars.cpp"
-reassign_unused_vars(file6, file7)
+files.append("out_files/newvars.cpp")
+reassign_unused_vars(files[-2], files[-1])
 t2 = time.time()
-print(f"Reassigned variables in {file6} in {t2 - t1} seconds")
+print(f"Reassigned variables in {files[-2]} in {t2 - t1} seconds")
 
 print("Removing unused indices...")
 t1 = time.time()
-file8 = "noskips2.cpp"
-remove_unused_vars(file7, file8)
+files.append("out_files/noskips2.cpp")
+remove_unused_vars(files[-2], files[-1])
 t2 = time.time()
-print(f"Removed unused indices from {file7} in {t2 - t1} seconds")
+print(f"Removed unused indices from {files[-2]} in {t2 - t1} seconds")
 
 print("Adding vectorization...")
 t1 = time.time()
-file9 = "poly-2b-v6x-new.cpp"
-vectorize_indices(file8, file9)
+file9 = "out_files/poly-2b-v6x-new.cpp"
+vectorize_indices(files[-2], files[-1])
 t2 = time.time()
-print(f"Vectorized {file8} in {t2 - t1} seconds")
+print(f"Vectorized {files[-2]} in {t2 - t1} seconds")
 
-size1 = how_big_is_it(file1)
-size2 = how_big_is_it(file9)
+size1 = how_big_is_it(files[0])
+size2 = how_big_is_it(files[-1])
 print(f"All done! Number of variables used decreased from {size1} to {size2}. Now fix the rest of the file yourself stupid")
--- optimize_3b_file.py
+++ optimize_3b_file.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 from computations import reassign_shared_computations
 from fix_indices import vectorize_indices
@@ -8,59 +9,62 @@ from remove_newlines import remove_newlines
 from remove_unused import remove_unused_vars
 from howBIGisit import how_big_is_it
 
+MBX_DIR = f"{os.getenv("HOME")}/MBX/src/potential"
+files = []
+
 print("Removing newlines...")
 t1 = time.time()
-file1 = "poly-2b-v6x-old.cpp"
-file2 = "nobreaks.cpp"
-remove_newlines(file1, file2)
+files.append(f"{MBX_DIR}/3b/poly-3b-v2x.cpp")
+files.append("out_files/nobreaks.cpp")
+remove_newlines(files[-2], files[-1])
 t2 = time.time()
-print(f"Removed newlines from {file1} in {t2 - t1} seconds")
+print(f"Removed newlines from {files[-2]} in {t2 - t1} seconds")
 
 print("Removing a assigments...")
 t1 = time.time()
-file3 = "noaassignments.cpp"
-remove_assignments(file2, file3, "a")
+files.append("out_files/noaassignments.cpp")
+remove_assignments(files[-2], files[-1], "a")
 t2 = time.time()
-print(f"Removed a assignments from {file2} in {t2 - t1} seconds")
+print(f"Removed a assignments from {files[-2]} in {t2 - t1} seconds")
 
 print("Removing unused indices...")
 t1 = time.time()
-file4 = "noskips.cpp"
-remove_unused_vars(file3, file4)
+files.append("out_files/noskips.cpp")
+remove_unused_vars(files[-2], files[-1])
 t2 = time.time()
-print(f"Removed unused indices from {file3} in {t2 - t1} seconds")
+print(f"Removed unused indices from {files[-2]} in {t2 - t1} seconds")
 
 print("Reassigning shared computations (pass 1)...")
 t1 = time.time()
-file5 = "sharedcomps.cpp"
-reassign_shared_computations(file4, file5)
+files.append("out_files/sharedcomps.cpp")
+reassign_shared_computations(files[-2], files[-1])
 print("Reassigning shared computations (pass 2)...")
-file6 = "sharedcomps2.cpp"
-reassign_shared_computations(file5, file6)
+files.append("sharedcomps2.cpp")
+reassign_shared_computations(files[-2], files[-1])
 t2 = time.time()
-print(f"Reassigned shared computations in {file4} in {t2 - t1} seconds")
+print(f"Reassigned shared computations in {files[-2]} in {t2 - t1} seconds")
 
 print("Reassigning variables...")
 t1 = time.time()
-file7 = "newvars.cpp"
-reassign_unused_vars(file6, file7)
+files.append("out_files/newvars.cpp")
+reassign_unused_vars(files[-2], files[-1])
 t2 = time.time()
-print(f"Reassigned variables in {file6} in {t2 - t1} seconds")
+print(f"Reassigned variables in {files[-2]} in {t2 - t1} seconds")
 
 print("Removing unused indices...")
 t1 = time.time()
-file8 = "noskips2.cpp"
-remove_unused_vars(file7, file8)
+files.append("out_files/noskips2.cpp")
+remove_unused_vars(files[-2], files[-1])
 t2 = time.time()
-print(f"Removed unused indices from {file7} in {t2 - t1} seconds")
+print(f"Removed unused indices from {files[-2]} in {t2 - t1} seconds")
 
 print("Adding vectorization...")
 t1 = time.time()
-file9 = "poly-2b-v6x-new.cpp"
-vectorize_indices(file8, file9)
+files.append("out_files/poly-3b-v2x-new.cpp")
+vectorize_indices(files[-2], files[-1])
 t2 = time.time()
-print(f"Vectorized {file8} in {t2 - t1} seconds")
+print(f"Vectorized {files[-2]} in {t2 - t1} seconds")
 
-size1 = how_big_is_it(file1)
-size2 = how_big_is_it(file9)
+size1 = how_big_is_it(files[0])
+size2 = how_big_is_it(files[-1])
 print(f"All done! Number of variables used decreased from {size1} to {size2}. Now fix the rest of the file yourself stupid")
